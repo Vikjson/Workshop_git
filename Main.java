724505d210f539a12a45f12576d8dff674fb8644
Sanna Ranta <sranta39@gmail.com>
2025-10-21 14:29:51 +0200

    Change Main.java text

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from person B again");
         System.out.println("Hello from Sanna");
+
+	System.out.println("Hello world");
     }
 }
