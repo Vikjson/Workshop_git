7556def1537aa67f0fa852a5ebb3a4de42a4bf53
Viktoria Johansson <viktoria.k.johansson@gmail.com>
2025-10-21 14:07:21 +0200

    Changed to Hello again from person B in Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Person A");
-        System.out.println("Hello from person B");
+        System.out.println("Hello from person B again");
 
     }
 }
